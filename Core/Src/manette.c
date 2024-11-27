ed39ee220ed70cb25f08696f99a62d351be178d8
YohanLefebvre <yoyolefebvre07@gmail.com>
2024-11-27 13:37:04 -0500

    Update manette.c

--- Core/Src/manette.c
+++ Core/Src/manette.c
@@ -1,12 +1,28 @@
 /*
  * manette.c
  *
- *  Created on: Oct 30, 2024
- *      Author: sebas
+ *  Created on: Nov 27, 2024
+ *      Author: Yohan Lefebvre
  */
 
 #include "manette.h"
 
 #define NUNCHUK_ADDRESS (0x52<<1)
+I2C_HandleTypeDef hi2c1;
+
+	uint8_t initcomm1[2] = {0xF0, 0x55};
+	uint8_t initcomm2[2] = {0xFB, 0x00};
+
+void Initialisation_manette(){
+
+
+
+
+
+	HAL_I2C_Master_Transmit(&hi2c1, NUNCHUK_ADDRESS, initcomm1, 2, HAL_MAX_DELAY);
+	HAL_Delay(10);
+	HAL_I2C_Master_Transmit(&hi2c1, NUNCHUK_ADDRESS, initcomm2, 2, HAL_MAX_DELAY);
+
+}
 
 
